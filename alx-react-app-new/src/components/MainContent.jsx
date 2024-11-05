7bff6d2bebc8524c7556c31f878983678fbae834
Melaku N <melaku.n@outlook.com>
2024-11-05 08:35:52 +0300

    Apply inline css styling

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{textAlign: "center"}}>
+      <p style={{color: "gold"}}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
