5f1a1406a8f4e36734a63e786cff1ff2bc9f729d
Melaku N <melaku.n@outlook.com>
2024-11-05 09:46:54 +0300

    Lorem ipsum dolor

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ background: "#333", padding: "10px" }}>
+    <nav style={{ backgroundColor: "#333", padding: "10px" }}>
       <ul
         style={{
           listStyle: "none",
