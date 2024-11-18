910848105fc22a9b3a30ab82f8b96a2495a89352
Melaku N <melaku.n@outlook.com>
2024-11-18 20:20:52 +0300

    What just happened?

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import UserProfile from './components/UserProfile';
+import NotFound from './components/NotFound';
+import { ProtectedRoute } from './components/ProtectedRoute';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+        <Route path="profile/:userId" element={<UserProfile />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
