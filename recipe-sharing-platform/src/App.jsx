79305c5d3dd1c63d4af29b388cba5e9f89d42cb3
Melaku N <melaku.n@outlook.com>
2024-12-08 18:18:17 +0300

    Setup

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="text-blue-500">
+      <h1 className="text-blue-500 font-bold underline">Welcome to the Recipe Sharing Platform!</h1>
+    </div>
+  );
+}
+
+export default App;
